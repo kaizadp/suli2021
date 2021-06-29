2acad2075f837532096d8a6fee54ddb0627c3d0a
kaizadp <50244730+kaizadp@users.noreply.github.com>
2021-06-28 22:07:33 -0700

    upload data and instructions for drought project

---
+++ tes_drought/code/0-instructions.R
@@ -0,0 +1,32 @@
+## SULI2021 summer project
+
+## Data from TES-Drought project
+## Investigating chemical (FTICR-MS, NMR) and microbial (16S) responses to drought.
+
+
+## Soil cores were collected from Caribou-Poker Creeks Research Watershed, CPCRW (Alaska) and Secret River, SR (WA). 
+## The "drought" cores were air dried and then incubated under drought conditions for 150 days.
+## The "timezero" cores were deconstructed and processed at the start of the incubation.
+
+## We will use the FTICRRR and NMRRR workflows to investigate shifts in the chemical profile for timezero vs. drought
+
+
+# 1. FTICR-MS
+## plot Van Krevelen graphs to describe the chemical composition for TZ and drought samples
+## which peaks were lost or gained during the drought? use Van Krevelen plots to report this.
+## calculate relative abundances of the different compound classes
+### -> report as table and bar graphs
+## perform statistical analyses to determine if Drought and Site had strong influences on chemical composition
+### -> PERMANOVA and PCA on multivariate data
+### -> ANOVA on univariate data (each individual class)
+
+# 2. NMR
+## we do not have much NMR data for the drought samples, only the TZ
+## calculate relative abundances of the different compound classes
+### -> report as table and bar graphs
+## perform statistical analyses to determine if Site had strong influences on chemical composition oif TZ samples
+### -> PERMANOVA and PCA on multivariate data
+### -> ANOVA on univariate data (each individual class)
+
+## How do the FTICR and NMR data compare to each other?
+
